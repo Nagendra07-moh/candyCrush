9bfde4ded4c22fbd41f355e42baa0b32746df3f8
Nagendra <nagendra.mohan.5@gmail.com>
2026-03-22 21:47:29 +0530

    sound in Progress

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, ImageBackground } from 'react-native'
+import Animated,{
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming
+} from 'react-native-reanimated'
+import React,{useEffect} from 'react'
+import { commonStyles } from '../styles/commonStyles'
+import { screenHeight, screenWidth } from '../utils/Constants'
+import { useIsFocused } from '@react-navigation/native'
 
-const HomeScreen = () => {
+const HomeScreen:React.FC = () => {
+
+  const isFocused = useIsFocused()
+  const translateY = useSharedValue(-200)
+
+  useEffect(()=>{
+      translateY.value = withTiming(0,{duration:3000})
+  },[isFocused])
+
+  const animatedStyle = useAnimatedStyle(()=>({
+    transform: [{translateY:translateY.value}]
+  }))
+
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+   <ImageBackground source={require('../assets/images/b2.png')} style={commonStyles.simpleContainer}>
+    <Animated.Image
+      source={require('../assets/images/banner.png')}
+      style={[styles.dropImage,animatedStyle]}
+    />
+   </ImageBackground>
   )
 }
+const styles = StyleSheet.create({
+  dropImage:{
+    width:screenWidth,
+    height:screenWidth * 0.8,
+    position:'absolute',
+    resizeMode:'contain',
+    top:-20
+  }
+})
 
 export default HomeScreen
